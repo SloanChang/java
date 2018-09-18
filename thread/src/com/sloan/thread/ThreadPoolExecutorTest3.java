a09043797a58b0ce83454410d0180e1abfc19a52
sloan <sloan@DESKTOP-5EPRNLO>
2018-09-18 17:23:55 +0800

    Thread init

---
+++ thread/src/com/sloan/thread/ThreadPoolExecutorTest3.java
@@ -0,0 +1,15 @@
+package com.sloan.thread;
+
+import java.util.concurrent.Executors;  
+import java.util.concurrent.ScheduledExecutorService;  
+import java.util.concurrent.TimeUnit;  
+public class ThreadPoolExecutorTest3 {  
+ public static void main(String[] args) {  
+  ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);  
+  scheduledThreadPool.schedule(new Runnable() {  
+   public void run() {  
+    System.out.println("delay 3 seconds");  
+   }  
+  }, 3, TimeUnit.SECONDS);  
+ }  
+} 
